e9063e52523df6c6bc25df529e429f1a909ef243
luphone04 <luphonemaw09@gmail.com>
2024-07-19 11:35:27 +0700

    KMean

--- KMean/color_quantization.py
+++ KMean/color_quantization.py
@@ -6,7 +6,7 @@ Z = img.reshape((-1,3))
 Z = np.float32(Z)
 # define criteria, number of clusters(K) and apply kmeans()
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 8
+K = 2
 ret,label,center=cv.kmeans(Z,K,None,criteria,10,cv.KMEANS_RANDOM_CENTERS)
 # Now convert back into uint8, and make original image
 center = np.uint8(center)
